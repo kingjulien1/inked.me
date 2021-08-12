9168218f39306d9960ebc694040cdc195c6c55de
julian-blaschke <julianblaschke@icloud.com>
2021-08-12 21:18:05 +0200

    animated shadow on navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,23 @@
-import Link from "next/link";
+import { useEventListener } from "hooks/useEventListener";
 import { signIn, useSession } from "next-auth/client";
+import Link from "next/link";
+import { useCallback, useState } from "react";
 
 const callbackUrl = `https://localhost:3000/profile`;
 
 export function Navbar() {
   const [session] = useSession();
+  const [isAtTopOfPage, setIsAtTopOfPage] = useState(true);
+  const scrollHandler = useCallback(() => setIsAtTopOfPage(window?.pageYOffset <= 0), [setIsAtTopOfPage]);
+  useEventListener("scroll", scrollHandler, typeof window != "undefined" && window);
 
   return (
-    <nav className="py-2 px-4 flex justify-between sticky text-sm">
+    <nav
+      className={[
+        "py-2 px-4 flex justify-between text-sm sticky top-0 bg-white dark:bg-black transition-shadow",
+        isAtTopOfPage ? null : "shadow-md",
+      ].join(" ")}
+    >
       <div>
         <Link href="/">
           <a className="mr-4">Home</a>
